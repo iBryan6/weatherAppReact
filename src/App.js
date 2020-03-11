4ceef7332ca2538ac1489b9cc09ccebf5108b83e
iBryan6 <bryan.argandona@Bryan-Argandona.local>
2020-03-11 18:13:50 -0400

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div>
+    <Header as='h1' content='WEATHER APP' textAlign='center' />
+    </div>
+  );
+}
+
+export default App;
