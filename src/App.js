fd494fb8b9c4d5f47d0d0e79d94a2afd145f5580
iBryan6 <bryan.argandona@Bryan-Argandona.local>
2020-03-12 12:13:48 -0400

    toogle to view on C or F

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Segment } from "semantic-ui-react";
+import { Header, Container } from "semantic-ui-react";
 import SearchCity from "./components/SearchCity/SearchCity";
 import Weather from "./components/Weather/Weather";
 import axios from "axios";
@@ -8,16 +8,22 @@ const REACT_APP_WEATHER_API_KEY = "91fae767180ad7427d046a4082c31887";
 export class App extends Component {
   state = {
     city: "",
-    temperature: undefined,
+    tempKelvin: undefined,
     humidity: undefined,
     information: "all",
     celsius: undefined,
     fahrenheit: undefined,
+    tempBool: false
   };
 
   //HANDLE FOR RADIO BUTTON [INFO/TEMP/HUM]
   infoHandler = (e, { value }) => this.setState({ information: value });
-  
+  //HANDLE FOR RADIO BUTTON [°C/°F]
+  tempHandler = () => {
+    this.setState({
+      tempBool: !this.state.tempBool
+    });
+  };
 
   //HANDLE TEXT CHANGE ON THE INPUT
   textHandler = e => {
@@ -33,10 +39,15 @@ export class App extends Component {
       )
       .then(res => {
         console.log(res.data.main);
-        let newCelsius = `${Math.round(res.data.main.temp - 273.15)*100/100} °C`;
-        let newFahrenheit = `${Math.round((res.data.main.temp - 273.15) * 1.8 + 32)*100/100} °F`;
+        let newCelsius = `${(Math.round(res.data.main.temp - 273.15) * 100) /
+          100} °C`;
+        let newFahrenheit = `${(Math.round(
+          (res.data.main.temp - 273.15) * 1.8 + 32
+        ) *
+          100) /
+          100} °F`;
         this.setState({
-          temperature: res.data.main.temp,
+          tempKelvin: res.data.main.temp,
           humidity: `${res.data.main.humidity} %`,
           celsius: newCelsius,
           fahrenheit: newFahrenheit
@@ -49,7 +60,7 @@ export class App extends Component {
 
   render() {
     return (
-      <Segment>
+      <Container>
         <Header as="h1" content="OKTANA WEATHER APP" textAlign="center" />
         <SearchCity
           getWeather={this.getWeather}
@@ -59,13 +70,15 @@ export class App extends Component {
         />
         <Weather
           city={this.state.city}
-          temperature={this.state.temperature}
+          tempKelvin={this.state.tempKelvin}
           humidity={this.state.humidity}
           info={this.state.information}
           celsius={this.state.celsius}
           fahrenheit={this.state.fahrenheit}
+          tempBool={this.state.tempBool}
+          tempHandler={this.tempHandler}
         />
-      </Segment>
+      </Container>
     );
   }
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,47 +1,73 @@
 import React from "react";
-import { Segment, Button, Icon, Header, Popup } from "semantic-ui-react";
+import {
+  Segment,
+  Button,
+  Icon,
+  Header,
+  Popup,
+  Checkbox
+} from "semantic-ui-react";
 
-const Weather = props => (
-  <div>
-    {props.temperature && props.humidity && (
-      <Segment style={{ marginTop: "40px" }}>
-        <Header sub>WEATHER RESULTS:</Header>
-        <br />
-        
-        {/* SHOW ALL */}
-        {props.temperature && props.humidity && props.info === "all" && (
-          <div>
-            <p>
-              <Icon name="cloud" size="large" /> Temperature: {props.fahrenheit}
-            </p>
-            <p>
-              <Icon name="shower" size="large" /> Humidity: {props.humidity}
-            </p>
-          </div>
-        )}
+const Weather = props => {
+  let temp = "";
+  props.tempBool === true ? (temp = props.fahrenheit) : (temp = props.celsius);
+  return (
+    <div>
+      {props.tempKelvin && props.humidity && (
+        <Segment style={{ marginTop: "40px" }}>
+          <Header sub>WEATHER RESULTS:</Header>
+          <br />
 
-        {/* SHOW JUST TEMP*/}
-        {props.temperature && props.humidity && props.info === "temp" && (
-          <div>
-            <p>
-              <Icon name="cloud" size="large" /> Temperature: {props.fahrenheit}
-            </p>
-          </div>
-        )}
+          {/* SHOW ALL */}
+          {props.tempKelvin && props.humidity && props.info === "all" && (
+            <div>
+              <div style={{ marginBottom: "20px" }}>
+                °C <Checkbox slider onChange={props.tempHandler} /> °F
+              </div>
+              <p>
+                <Icon name="cloud" size="large" /> Temperature:{temp}
+              </p>
+              <p>
+                <Icon name="shower" size="large" /> Humidity: {props.humidity}
+              </p>
+            </div>
+          )}
 
-        {/* SHOW JUST HUM*/}
-        {props.temperature && props.humidity && props.info === "hum" && (
-          <div>
-            <p>
-              <Icon name="shower" size="large" /> Humidity: {props.humidity}
-            </p>
-          </div>
-        )}
+          {/* SHOW JUST TEMP*/}
+          {props.tempKelvin && props.humidity && props.info === "temp" && (
+            <div>
+              <div style={{ marginBottom: "20px" }}>
+                °C <Checkbox slider onChange={props.tempHandler} /> °F
+              </div>
+              <p>
+                <Icon name="cloud" size="large" /> Temperature:{temp}
+              </p>
+            </div>
+          )}
 
-        <Popup content='Add this city to your favorites' trigger={<Button icon='star' color="yellow" style={{ marginTop: "20px" }}/>} />
-      </Segment>
-    )}
-  </div>
-);
+          {/* SHOW JUST HUM*/}
+          {props.tempKelvin && props.humidity && props.info === "hum" && (
+            <div>
+              <p>
+                <Icon name="shower" size="large" /> Humidity: {props.humidity}
+              </p>
+            </div>
+          )}
+
+          <Popup
+            content="Add this city to your favorites"
+            trigger={
+              <Button
+                icon="star"
+                color="yellow"
+                style={{ marginTop: "20px" }}
+              />
+            }
+          />
+        </Segment>
+      )}
+    </div>
+  );
+};
 
 export default Weather;
